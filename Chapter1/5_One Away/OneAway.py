6a2e305623a212edfd25c365d10c534740cdc679
Frank Rose <franklin.rose@gmail.com>
2020-07-13 22:08:25 -0400

    Reset One Away

--- Chapter1/5_One Away/OneAway.py
+++ Chapter1/5_One Away/OneAway.py
@@ -6,31 +6,6 @@ def one_away(s1, s2):
     pass
 
 
-def one_edit_replace(s1, s2):
-    edited = False
-    for c1, c2 in zip(s1, s2):
-        if c1 != c2:
-            if edited:
-                return False
-            edited = True
-    return True
-
-
-def one_edit_insert(s1, s2):
-    edited = False
-    i, j = 0, 0
-    while i < len(s1) and j < len(s2):
-        if s1[i] != s2[j]:
-            if edited:
-                return False
-            edited = True
-            j += 1
-        else:
-            i += 1
-            j += 1
-    return True
-
-
 class Test(unittest.TestCase):
     '''Test Cases'''
     data = [
